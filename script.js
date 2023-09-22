95b31916ab3ecd3c8670838c379b9bb01264f962
Adrian Graça <adrianspk1@gmail.com>
2023-09-22 20:07:29 -0300

    sorteio numerico done

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+var button = document.getElementById('generate')
 
+button.addEventListener('click', function() {
+    var min = parseInt(document.getElementById('min').value)
+    var max = parseInt(document.getElementById('max').value)
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min
+
+    document.querySelector('#result > span').textContent = result
+}
+) 
 
